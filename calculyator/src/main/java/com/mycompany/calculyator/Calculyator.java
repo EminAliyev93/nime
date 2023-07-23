12701fc8e89181e696a5a9c7d60ff06364bdb6fd
EminAliyev93 <eminemin99335@gmail.com>
2023-07-24 02:54:30 +0400

    calculator

---
+++ calculyator/src/main/java/com/mycompany/calculyator/Calculyator.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculyator;
+
+/**
+ *
+ * @author Emin
+ */
+public class Calculyator {
+
+    public static void main(String[] args) {
+        System.out.println("hello vords");
+        
+     }
+     public static double topla(double a, double b,){
+         return a+b,
+     }    
+     }
+             
+   
+
